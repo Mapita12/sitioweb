bb313c5e0819c96cdb941d37cd9349c6344b6c77
Maria Patricia <mapasaal7777@gmail.com>
2024-06-10 18:14:03 -0500

    actualizando

--- main.py
+++ main.py
@@ -9,8 +9,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print_hi('Hola PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
